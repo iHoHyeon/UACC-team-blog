1abef1212d4d9f76a420e79a2cda0e0b7bcc8688
iHoHyeon <hyunee169@naver.com>
2022-05-28 00:03:28 +0900

    add Projects page information

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,6 +1,6 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
+    title: '(Example) A Search Engine',
     description: `What if you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
     for.`,
@@ -8,7 +8,7 @@ const projectsData = [
     href: 'https://www.google.com',
   },
   {
-    title: 'The Time Machine',
+    title: '(Example) The Time Machine',
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten headphones with this simple yet affordable solution.`,
