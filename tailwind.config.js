7fb6c7e099dc5cd1c056c0e58a2425a8def78d63
iHoHyeon <hyunee169@naver.com>
2022-05-28 15:39:32 +0900

    change theme primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
         sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: colors.teal,
+        primary: colors.blue,
         gray: colors.neutral,
       },
       typography: (theme) => ({
